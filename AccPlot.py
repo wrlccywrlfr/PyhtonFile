01e923dbd10d85c88785c0c232f713579a4ba8fa
wrlccywrlfr <diskwtnb.dis.poke@outlook.jp>
2020-10-13 16:00:20 +0900

    次回は計算式を適応する

--- AccPlot.py
+++ AccPlot.py
@@ -5,7 +5,7 @@ import math
 from scipy import signal
 from scipy.signal import find_peaks
 
-filename = './data1/d-12.5-1A.csv'
+filename = './data1/c-12.5-1A.csv'
 AccX = []
 GyroZ = []
 
@@ -25,8 +25,13 @@ def SourcePlot():#グラフの表示
         time = []
         a = 0
         reader = csv.reader(f)
-        #next(reader)
-        #next(reader)
+        # for row in reader:
+        #     if row[0] == "ags":
+        #         t.append(float(row[1]))
+        #         AccX.append(float(row[2]))
+        #         GyroZ.append(float(row[7]))
+        #         time.append(a)
+        #         a = a + 1
 
         for row in reader:
             if row[0] == "1":
@@ -59,6 +64,7 @@ def Analysis(): #1000切ったらスタート
     dis = 0
     SumDistance = 0
     SumAngle = 0
+    count = 0
 
     with open(filename) as f:
         reader = csv.reader(f)
@@ -71,35 +77,39 @@ def Analysis(): #1000切ったらスタート
     for AccValue in AccX:
         #速度計算
         Speed = ((OldAcc + AccValue) * TimeSpan) /2 
-        OldAcc = AccValue
+        OldAcc = AccValue #現在の加速度を保存
 
         #距離計算
         Distance = ((Speed + OldSpeed) * TimeSpan) /2 #瞬間の距離
-        OldSpeed = Speed
+        OldSpeed += Speed #現在の速度を保存(速度は加算？)
 
         #角度推定
         GyroValue = GyroZ[num]
         num += 1
         #度数法をラジアンに変換
-        GyroValue = math.radians(GyroValue)
+        GyroValue = math.radians(GyroValue)#DPSだから度数法
         #角度計算
-        Angle = ((OldAngle + GyroValue) *TimeSpan) /2
+        Angle = ((OldAngle + GyroValue) *TimeSpan) /2 
         #角度加算
-        OldAngle = SumAngle
+        OldAngle = Angle
         SumAngle += Angle
         
 
-        dis = Distance/math.cos(SumAngle)  
-        SumDistance = SumDistance + dis
-        #print(SumDistance)
+        dis = Distance/math.cos(SumAngle) #瞬間の距離
+        SumDistance = SumDistance + dis #距離の加算
 
     
     SumDistance = SumDistance
-    print(SumDistance)
+    print(SumDistance*0.001)
+#Memo
+#加速度の単位は0.1mg(1g = 9.8m/S^2)(0.1mgは1/10000)
+#10000 = 1g
+#10000 * (9.8/10000) = 9.8m/s^2
+#角速度は0.01dps(1dpsは1秒で1度)(4000は1秒で40度)
                     
 def main():
-    #SourcePlot()
-    Analysis()
+    SourcePlot()
+    #Analysis()
 
 if __name__ == "__main__":
     main()
